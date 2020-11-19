804843cede7c6a29f55eb6b8d82b02b28bc3f997
Jesus Ortega <jeortegaga@gmail.com>
2020-11-19 01:02:12 -0500

    cambios menores

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -8,7 +8,7 @@
 </style>
 <div class="card uper">
   <div class="card-header">
-    Edit Corona Virus Data
+   Editar datos de la persona
   </div>
   <div class="card-body">
     @if ($errors->any())
